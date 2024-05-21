287ba91d1f3646c4a4f6c33eb7cc56db7a83c024
Juan manuel Pedro Villalba <xomie_01@hotmail.com>
2024-05-21 19:46:41 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,67 +1,95 @@
 jQuery(document).ready(function($) {
-    console.log('Mi Widget de Escritorio está activo.');
- function prepareChartData(data) {
+function prepareChartData(data, period) {
     const labels = [];
     const pageviewsData = [];
     const visitorsData = [];
-    const last7Days = data["7_days"];
+    let periodData = null;
 
-    last7Days.pageviews.data.sort((a, b) => new Date(a.value) - new Date(b.value));
-    last7Days.visitors.data.sort((a, b) => new Date(a.value) - new Date(b.value));
-    for (const item of last7Days.pageviews.data) {
-        const date = new Date(item.value);
-        const dayOfMonth = date.getDate();
-        const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-        const monthName = monthNames[date.getMonth()];
-        labels.push(`${dayOfMonth}/${monthName}`);
-        pageviewsData.push(parseInt(item.count));
-    }
+    if (data[period] && data[period].pageviews && data[period].pageviews.data && data[period].visitors && data[period].visitors.data) {
+        periodData = data[period];
+        periodData.pageviews.data.sort((a, b) => new Date(a.value) - new Date(b.value));
+        periodData.visitors.data.sort((a, b) => new Date(a.value) - new Date(b.value));
 
-    for (const item of last7Days.visitors.data) {
-        visitorsData.push(parseInt(item.count));
+        for (const item of periodData.pageviews.data) {
+            const date = new Date(item.value);
+            const dayOfMonth = date.getDate();
+            const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+            const monthName = monthNames[date.getMonth()];
+            labels.push(`${dayOfMonth}/${monthName}`);
+            pageviewsData.push(parseInt(item.count));
+        }
+
+        for (const item of periodData.visitors.data) {
+            visitorsData.push(parseInt(item.count));
+        }
+    } else {
+        console.error(`Datos insuficientes para el periodo ${period}.`);
     }
 
     return { labels, pageviewsData, visitorsData };
 }
 
-const chartData = prepareChartData(statsData);
-
-const chartOptions = {
-    plugins: {
-        title: {
-            display: true,
-            text: 'Pageviews and Visitors for the Last 7 Days'
-        }
-    },
-    scales: {
-        y: {
-            stacked: false
-        }
+dataChart.chartService = (days) => {
+    let chartData = null;
+    const chartDataFor7Days = prepareChartData(statsData, "7_days");
+    const chartDataFor15Days = prepareChartData(statsData, "15_days");
+    const chartDataFor30Days = prepareChartData(statsData, "30_days");
+    
+    
+    if (days === 7) {
+        chartData = chartDataFor7Days;
+    } else if (days === 15) {
+        chartData = chartDataFor15Days;
+    } else if (days === 30) {
+        chartData = chartDataFor30Days;
+    } else {
+        console.error("Número de días no válido."); 
+        return;
     }
+
+    const chartOptions = {
+        plugins: {
+            title: {
+                display: true,
+                text: 'Pageviews and Visitors for the Last ' + days + ' Days'
+            }
+        },
+        scales: {
+            y: {
+                stacked: false
+            }
+        }
+    };
+
+    const ctx = document.getElementById('canvas').getContext('2d');
+    if (window.myChart) {
+        window.myChart.destroy();
+    }
+
+    window.myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: chartData.labels,
+            datasets: [
+                {
+                    label: 'Pageviews',
+                    data: chartData.pageviewsData,
+                    borderColor: 'rgba(255, 177, 160, 1)',
+                    backgroundColor: 'rgba(255, 87, 51, 0.9)',
+                    borderWidth: 1
+                },
+                {
+                    label: 'Visitors',
+                    data: chartData.visitorsData,
+                    borderColor: 'rgba(0, 135, 218, 1)',
+                    backgroundColor: 'rgba(82, 187, 252, 0.9)',
+                    borderWidth: 1
+                }
+            ]
+        },
+        options: chartOptions
+    });
 };
-
-const ctx = document.getElementById('canvas').getContext('2d');
-
-const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: chartData.labels,
-        datasets: [{
-            label: 'Pageviews',
-            data: chartData.pageviewsData,
-            borderColor: 'rgba(255, 177, 160, 1)',
-            backgroundColor: 'rgba(255, 87, 51, 0.9)',
-            borderWidth: 1
-        }, {
-            label: 'Visitors',
-            data: chartData.visitorsData,
-            borderColor: 'rgba(0, 135, 218, 1)',
-            backgroundColor: 'rgba(82, 187, 252, 0.9)',
-            borderWidth: 1
-        }]
-    },
-    options: chartOptions
-});
-         
+    dataChart.chartService(7); 
 });
 
